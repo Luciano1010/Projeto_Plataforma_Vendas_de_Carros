4c98e6cd90f6b7bd7c3380960cd37238bcd379e8
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2024-02-07 08:33:30 -0300

    criando context - authContext

--- plataforma/src/components/header/index.tsx
+++ plataforma/src/components/header/index.tsx
@@ -1,11 +1,13 @@
+import { useContext } from 'react';
+import { authContext } from '../../context/authContext';
 import { Link } from 'react-router-dom';
 import logoimg from '../../assets/logo.svg'
 import { FiLogIn, FiUser } from 'react-icons/fi';
 
 export function Header() {
-    const signed = false;
-    const loadingAuth = false;
-    return (
+   const {signed, loadingAuth} = useContext(authContext)
+    
+   return (
         <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
             <header className='flex w-full max-w-7xl items justify-between px-4 mx-4'>
                 <Link to={'/'}>
@@ -15,7 +17,7 @@ export function Header() {
                     />
                 </Link>
                 {!loadingAuth && signed && (
-                    <Link to={'/dashbord'}>
+                    <Link to={'/dashboard'}>
                         <div className='border-2 rounded-full p-1 border-gray-900'>
                             <FiUser size={20} color='#000' />
                         </div>
